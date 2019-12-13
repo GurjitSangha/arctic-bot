be7d041759887df08d2d8d1b4eb0a11faadbf3bb
Gurjit Sangha <gurjit.sangha@sky.uk>
2019-12-13 22:00:58 +0000

    shade

--- bot.js
+++ bot.js
@@ -4,6 +4,7 @@ const config = require('./config')
 const bot = controller.spawn({
     token: config('SLACK_TOKEN')
 })
+const axios = require('axios')
 
 bot.startRTM((err, bot, payload) => {
     if (err) {
@@ -12,10 +13,10 @@ bot.startRTM((err, bot, payload) => {
 
     controller.hears(['echo'], ['direct_mention'], (bot, message) => {
         console.log(`echo received: ${message.text}`)
-        var split = message.text.split(' ');
+        var split = message.text.split(' ')
         // remove 'echo' and join the remaining elements
-        split.shift();
-        bot.reply(message, split.join(' '));
+        split.shift()
+        bot.reply(message, split.join(' '))
     })
 
     controller.hears(['8ball!'], ['ambient'], function(bot, message) {
@@ -43,8 +44,36 @@ bot.startRTM((err, bot, payload) => {
           'Very doubtful'
         ];
     
-        var answer = answers[Math.floor(Math.random() * answers.length)];
+        var answer = answers[Math.floor(Math.random() * answers.length)]
     
-        bot.reply(message, answer);
+        bot.reply(message, answer)
     });
+
+    controller.hears(['shade!'], ['direct_mention'], (bot, message) => {
+        var split = message.text.split(' ')
+        split.shift()
+        split.forEach(async userStr => {
+            const userId = userStr.substring(2, userStr.length - 1)
+            const slackUrl = `https://slack.com/api/users.info?token=${config('SLACK_TOKEN')}&user=${userId}`
+            const slackResponse = await axios.get(slackUrl)
+            const name = slackResponse.data.user.real_name
+            console.log(`New shade point for ${name}!`)
+        
+            const getResponse = await axios.get(config('JSON_BIN_URL'))
+            const scores = getResponse.data
+        
+            if (name in scores) {
+                scores[name] = scores[name] + 1
+            } else {
+                scores[name] = 1
+            }
+            console.log(scores)
+            
+            const putResponse = await axios.put(config('JSON_BIN_URL'), scores)
+            if (putResponse.data) {
+                const msg = `New shade point for ${name}! Their total is now ${scores[name]}`
+                bot.reply(message, msg)
+            }
+        })
+    })
 })
--- config.js
+++ config.js
@@ -7,7 +7,8 @@ var config = {
     ENV: process.env.NODE_ENV,
     PORT: process.env.PORT,
     WEBHOOK_URL: process.env.WEBHOOK_URL,
-    SLACK_TOKEN: process.env.SLACK_TOKEN
+    SLACK_TOKEN: process.env.SLACK_TOKEN,
+    JSON_BIN_URL: process.env.JSON_BIN_URL
 }
 
 module.exports = (key) => {
